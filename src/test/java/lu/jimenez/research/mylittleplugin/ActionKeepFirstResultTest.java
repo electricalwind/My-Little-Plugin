3c4394e8244cea7dc10ea3eb37d597064dee99ab
Matthieu Jimenez <matthieu@jimenez.lu>
2016-11-25 16:38:15 +0100

    keep first test

--- src/test/java/lu/jimenez/research/mylittleplugin/ActionKeepFirstResultTest.java
+++ src/test/java/lu/jimenez/research/mylittleplugin/ActionKeepFirstResultTest.java
@@ -1,12 +1,34 @@
 package lu.jimenez.research.mylittleplugin;
 
 import org.junit.jupiter.api.Test;
+import org.mwg.task.ActionFunction;
+import org.mwg.task.TaskContext;
 
-class ActionKeepFirstResultTest extends ActionTest{
+import static lu.jimenez.research.mylittleplugin.MyLittleActions.keepFirstResult;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mwg.core.task.Actions.get;
+import static org.mwg.core.task.Actions.readGlobalIndexAll;
+import static org.mwg.core.task.Actions.task;
+
+class ActionKeepFirstResultTest extends ActionTest {
 
     @Test
-    public void test(){
-
+    public void test() {
+        initGraph();
+        task().then(readGlobalIndexAll("nodes"))
+                .then(get("children"))
+                .thenDo(new ActionFunction() {
+                    public void eval(TaskContext context) {
+                        assertEquals(context.result().size(),2);
+                    }
+                })
+                .then(keepFirstResult())
+                .thenDo(new ActionFunction() {
+            public void eval(TaskContext context) {
+                assertEquals(context.result().size(),1);
+            }
+        }).execute(graph,null);
+        removeGraph();
     }
 
 }
