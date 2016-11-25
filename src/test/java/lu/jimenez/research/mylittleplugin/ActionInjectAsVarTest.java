7439f2a8cc9738bbe322352b5131643c36c070bf
Matthieu Jimenez <matthieu@jimenez.lu>
2016-11-25 16:59:28 +0100

    inject as var

--- src/test/java/lu/jimenez/research/mylittleplugin/ActionInjectAsVarTest.java
+++ src/test/java/lu/jimenez/research/mylittleplugin/ActionInjectAsVarTest.java
@@ -1,12 +1,27 @@
 package lu.jimenez.research.mylittleplugin;
 
 import org.junit.jupiter.api.Test;
+import org.mwg.task.ActionFunction;
+import org.mwg.task.TaskContext;
 
-class ActionInjectAsVarTest extends ActionTest{
+import static lu.jimenez.research.mylittleplugin.MyLittleActions.injectAsVar;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mwg.core.task.Actions.task;
+
+class ActionInjectAsVarTest extends ActionTest {
 
     @Test
     public void test() {
-
+        initGraph();
+        task()
+                .then(injectAsVar("myvar", 1))
+                .thenDo(new ActionFunction() {
+                    public void eval(TaskContext context) {
+                        assertEquals(context.variable("myvar").get(0), 1);
+                    }
+                })
+                .execute(graph, null);
+        removeGraph();
     }
 
 }
